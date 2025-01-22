e267649be3544c6db96ac86ce32f6332b3f6d3d2
shoyeb reza <shoyebreza@gmail.com>
2025-01-22 20:59:00 +0600

    module 7

--- M7/array.php
+++ M7/array.php
@@ -1,7 +1,13 @@
 <?php
 
+$fruits  = array("a"=>"Apple","B"=>"Banana","C"=>"Comola Lebu","O"=>"Orange","P"=>"Pineapple",
+"J"=>"Jack Fruit");
 
+$numbers = array(1,2,3,4,5,6,7,8,9,10,11,12,13) ;
 
+if(in_array(4, $numbers)) {
+    echo "4 founded";
+}
 
 
 
---
+++ M7/tempCodeRunnerFile.php
@@ -0,0 +1,4 @@
+<?php
+if(in_array(4, $numbers)) {
+    echo "4 founded";
+}
