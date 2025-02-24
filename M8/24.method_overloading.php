745203e51a95acef8529cf71bf04b01ff0e19fa1
shoyeb reza <shoyebreza@gmail.com>
2025-02-24 21:10:39 +0600

    method over loading

---
+++ M8/24.method_overloading.php
@@ -0,0 +1,79 @@
+<?php 
+class MotorCycle{
+    private $parameter;
+    function __construct($displacement, $capacity, $mileage)
+    {
+        $this->parameter = [];
+        $this->parameter['displacement'] = $displacement;
+        $this->parameter['capacity'] = $capacity;
+        $this->parameter['mileage'] = $mileage;
+    }
+    /* function getDisplacement()
+    {
+        return $this->parameter['displacement'];
+    }
+    function setDisplacement($displacement)
+    {
+        $this->parameter['displacement'] = $displacement;
+    } */
+    
+    function __isset($name){
+        if (!isset($this->parameter[$name])) {
+            echo"{$name} not found\n";
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function __unset($name){
+        print_r($this->parameter);
+
+        unset($this->parameter[$name]);
+
+        print_r($this->parameter);
+    }
+
+    function __get( $name )
+    {
+        echo $this->parameter[$name];
+    }
+    function __set( $name, $value )
+    {
+        echo $this->parameter[$name] = $value;
+    }
+
+    function __call( $name, $arguments ){
+        if('run' == $name){
+            if($arguments){
+                echo"I am running at {$arguments[0]}";
+            } else {
+                echo"I am running ";
+            }
+
+        }
+    }
+
+    static function __callStatic($name, $arguments){
+        echo "Static call";
+        
+    }
+
+    
+}
+
+
+MotorCycle::wash();
+$pulsar = new MotorCycle('150cc','16ltr','45kmph');
+echo $pulsar->displacement;
+
+if(isset($pulsar->tiresize)){
+    echo "Found\n";
+} else {
+    echo "Not found\n";
+}
+
+unset($pulsar->mileage);
+$pulsar->run('100kmph');
+
+?>
